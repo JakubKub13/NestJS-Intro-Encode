40107731ec41f06b9a60acd67c2b1e0040045687
JK1331 <jakubkubala3@gmail.com>
2022-10-13 13:05:26 +0200

    Solving bugs

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { ethers } from 'ethers';
 import * as dotenv from "dotenv";
+import * as TokenJson from "./assets/MyERC20Vote.json";
 
 const CONTRACT_ADDRESS = "0x9828c2Ad0A705F3E8D21FE31A1a5edBFDfc67e1f"; 
 
 @Injectable()
 export class AppService {
-  getTotalSupply() {
-    const provider = new ethers.providers.JsonRpcProvider(process.env.GOERLI_RPC_URL);
-    const contract = new ethers.Contract(CONTRACT_ADDRESS, interface, provider)  
+  async getTotalSupply() {
+    const provider = ethers.providers.getDefaultProvider("goerli");
+    const contract = new ethers.Contract(CONTRACT_ADDRESS, TokenJson.abi, provider);  
+    const totalSupply = await contract.totalSupply();
+    return totalSupply;
   }
 }
