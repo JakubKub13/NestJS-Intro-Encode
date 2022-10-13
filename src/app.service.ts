13825980f71d123917f5d5c214b0874fea89460c
JK1331 <jakubkubala3@gmail.com>
2022-10-13 14:01:19 +0200

    Working on post methods

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,14 +5,23 @@ import * as TokenJson from "./assets/MyERC20Vote.json";
 
 const CONTRACT_ADDRESS = "0x9828c2Ad0A705F3E8D21FE31A1a5edBFDfc67e1f"; 
 
+export class PaymentOrder {
+  id: string;
+  secret: string;
+  amount: number;
+}
+
 @Injectable()
 export class AppService {
   provider: ethers.providers.Provider;
   contract: ethers.Contract;
 
+  database: PaymentOrder[];
+
   constructor() {
     this.provider = ethers.providers.getDefaultProvider("goerli");
     this.contract = new ethers.Contract(CONTRACT_ADDRESS, TokenJson.abi, this.provider);  
+    this.database = [];
   }
   async getTotalSupply() {
     const totalSupplyBn = await this.contract.totalSupply();
@@ -24,5 +33,18 @@ export class AppService {
     const allowanceBn = await this.contract.allowance(from, to);
     const allowance = ethers.utils.formatEther(allowanceBn);
     return allowance;
+  }
+
+  getTransactionByHash(hash: string) {
+    return this.provider.getTransaction(hash);
+  }
+
+  async getTransactionReceiptByHash(hash: string) {
+    const transaction = await this.getTransactionByHash(hash);
+    return await transaction.wait();
+  }
+
+  createPaymentOrder(body: PaymentOrder) {
+    this.database.push(body);
   }
 }
