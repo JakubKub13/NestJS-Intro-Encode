3aa4bc1d93e23aa53cbe528f98dce2b135281afe
JK1331 <jakubkubala3@gmail.com>
2022-10-13 12:35:58 +0200

    Working on app.service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { ethers } from 'ethers';
+import * as dotenv from "dotenv";
 
 const CONTRACT_ADDRESS = "0x9828c2Ad0A705F3E8D21FE31A1a5edBFDfc67e1f"; 
 
 @Injectable()
 export class AppService {
   getTotalSupply() {
-    throw new Error('Method not implemented.');
+    const provider = new ethers.providers.JsonRpcProvider(process.env.GOERLI_RPC_URL);
+    const contract = new ethers.Contract(CONTRACT_ADDRESS, interface, provider)  
   }
 }
