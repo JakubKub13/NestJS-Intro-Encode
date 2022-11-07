74da2f8349331ac319cdb2b9cbc28b7ae76e6ae1
JakubKub13 <jakubkubala3@gmail.com>
2022-11-07 17:25:36 +0100

    Refactoring app.service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,99 +1,16 @@
-import { Controller, Get, Param, Query, Post, Body } from '@nestjs/common';
-import { AppService, PaymentOrder, ClaimPaymentDTO, VotePower, Mint, CastVote} from './app.service';
+import { Controller, Get, Param, BadGatewayException, Post, Body } from '@nestjs/common';
+import { AppService } from './app.service';
+import isValidAddress from './helpers/isValidAddress';
+import { MintVotingTokensDto } from './polls/dto/mint-voting-tokens.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('token-address')
-  getTokenAddress() {
-    return this.appService.getTokenAddress();
+  // Get token contract address
+  @Get('token-contract-address')
+  getTokenContractAddress() {
+    return this.appService.getTokenContractAddress();
   }
-
-  @Get('total-supply')
-  getTotalSupply() {
-    return this.appService.getTotalSupply();
-  }
-
-  @Get('allowance')
-  getAllowance(@Query("from") from: string, @Query("to") to: string) {
-    return this.appService.getAllowance(from, to);
-  }
-
-  @Post('create-order')
-  createOrder(@Body() body: PaymentOrder ) {
-    console.log({body});
-    this.appService.createPaymentOrder(body);
-  }
-
-  @Post('mint')
-  mint(@Body() body: Mint) {
-    return this.appService.mint(body);
-  }
-
-  @Post('claim-payment')
-  claimPayment(@Body() body: ClaimPaymentDTO) {
-    return this.appService.claimPayment(body);
-  }
-
-  @Get('get-payment-order')
-  getPaymentOrder(@Query("id") id: string) {
-    return this.appService.getPaymentOrderById(id);
-  }
-
-  @Get('list-payment-orders')
-  listPaymentOrders() {
-    return this.appService.listPaymentOrders();
-  }
-
-  @Get('vote-power')
-  getVotePower(@Query("address") address: string) {
-    return this.appService.getVotePower(address);
-  }
-
-  @Get('vote-number')
-  getVote(@Query("address") address: string) {
-    return this.appService.getVote(address);
-  }
-
-  @Get('vote-spent') 
-  votePowerSpent(@Query("address") address: string) {
-    return this.appService.votePowerSpent(address);
-  }
-
-  @Get('proposal')
-  getProposal() {
-    return this.appService.getProposal()
-  }
-
-  @Post('delegate')
-  delegate(@Body() body: VotePower) {
-    return this.appService.delegate(body);
-  }
-
-  @Post('vote')
-  postVote(@Body() body: CastVote) {
-    return this.appService.postVote(body);
-  }
-
-  @Get('transaction-by-hash/:hash')
-  getTransactionByHash(@Param('hash') hash: string) {
-    return this.appService.getTransactionByHash(hash);
-  }
-
-  @Get('transaction-receipt-by-hash/:hash')
-  getTransactionReceiptByHash(@Param('hash') hash: string) {
-    return this.appService.getTransactionReceiptByHash(hash);
-  }
-
-  @Post('request-voting-tokens')
-  request(@Body() body: any) {
-    return this.appService.requestTokens(body);
-  }
-
-  @Get('ballot-address')
-  getBallotAddress() {
-    return this.appService.getBallotAddress();
-  }  
 }
 
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,178 +1,33 @@
-import { HttpException, Injectable } from '@nestjs/common';
-import { ethers } from 'ethers';
-import * as TokenJson from "./assets/MyERC20Vote.json";
-import * as BallotJson from "./assets/TokenizedBallot.json";
-require('dotenv').config();
+import { Model } from "mongoose";
+import { Injectable, InternalServerErrorException, BadRequestException } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Voter, VoterDocument } from "./schemas/voter.schema";
+import { MintVotingTokensDto } from "./polls/dto/mint-voting-tokens.dto";
+import { ethers } from "ethers";
+import * as tokenContractAddressJSON from '../src/assets/MyERC20Vote.address.json';
+import * as MyERC20JSON from "../src/assets/MyERC20Vote.json";
+import * as dotenv from 'dotenv';
+dotenv.config();
+import isMintingAllowed from "./helpers/isMintingAllowed";
+import currentEpoch from "./helpers/currentEpoch";
+import mintTokens from "./assets/mintTokens.helper";
 
-const CONTRACT_ADDRESS = "0x9828c2Ad0A705F3E8D21FE31A1a5edBFDfc67e1f"; 
-const CONTRACT_BALLOT_ADDRESS = "0x7EC3AcF0612aD269B7d338c5a90f58fc21c665B3";
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-
-export class PaymentOrder {
-  id: string;
-  secret: string;
-  amount: number;
-}
-
-export class Proposal {
-  name: string;
-  voteCount: string;
-}
-
-export class ClaimPaymentDTO {
-  id: string;
-  secret: string;
+type VoterTypeLocal = {
   address: string;
-}2
-
-export class Mint {
-  address: string;
-  amount: string;
-}
-export class CastVote {
-  proposalIndex: number;
-  amount: string;
-}
-
-export class VotePower {
-  address: string;
+  lastMinEpoch: number;
+  save?: () => {};
 }
 
 @Injectable()
 export class AppService {
-  provider: ethers.providers.Provider;
+  provider: ethers.providers.JsonRpcProvider;
   contract: ethers.Contract;
-  ballContract: ethers.Contract;
-  database: PaymentOrder[];
-  wallet: ethers.Wallet;
-  proposal: Proposal[];
 
-  constructor() {
-    this.provider = ethers.providers.getDefaultProvider("goerli");
-    this.contract = new ethers.Contract(CONTRACT_ADDRESS, TokenJson.abi, this.provider);
-    this.ballContract = new ethers.Contract(CONTRACT_BALLOT_ADDRESS, BallotJson.abi, this.provider)  
-    this.database = [];
-    this.wallet = new ethers.Wallet(PRIVATE_KEY, this.provider);
-    this.proposal = [];
+  constructor(@InjectModel(Voter.name) private voterName: Model<VoterDocument>) {
+    this.provider = new ethers.providers.JsonRpcProvider(process.env.GOERLI_RPC_URL);
+    this.contract = new ethers.Contract(tokenContractAddressJSON["token-goerli-address"], MyERC20JSON.abi, this.provider);
   }
 
-  getTokenAddress() {
-    return {result: CONTRACT_ADDRESS};
-  }
-
-  async getTotalSupply() {
-    const totalSupplyBn = await this.contract.totalSupply();
-    const totalSupply = ethers.utils.formatEther(totalSupplyBn);
-    return {result: totalSupply};
-  }
-
-  async getAllowance(from: string, to: string) {
-    const allowanceBn = await this.contract.allowance(from, to);
-    const allowance = ethers.utils.formatEther(allowanceBn);
-    return allowance;
-  }
-
-  getTransactionByHash(hash: string) {
-    return this.provider.getTransaction(hash);
-  }
-
-  async getTransactionReceiptByHash(hash: string) {
-    const transaction = await this.getTransactionByHash(hash);
-    return await transaction.wait();
-  }
-
-  createPaymentOrder(body: PaymentOrder) {
-    this.database.push(body);
-  }
-
-  getPaymentOrderById(id: string) {
-    const element = this.database.find((entry) => entry.id === id);
-    if (!element) throw new HttpException("Not Found", 404);
-    return { id: element.id, amount: element.amount };
-  }
-
-  listPaymentOrders() {
-    const filteredDatabase = [];
-    this.database.forEach(element => {
-      filteredDatabase.push({id: element.id, amount: element.amount});
-    });
-    return filteredDatabase;
-  }
-
-  async claimPayment(body: ClaimPaymentDTO) {
-    const privateKey1 = process.env.PRIVATE_KEY;
-    const element = this.database.find((entry) => entry.id === body.id);
-    if (!element) throw new HttpException("Not Found", 404);
-    if (body.secret != element.secret) return false;
-    const wallet = new ethers.Wallet(privateKey1, this.provider);
-    const signedContract = this.contract.connect(wallet)
-    // Todo mint tokens here
-    const tx = await signedContract.mint(body.address, ethers.utils.parseEther(element.amount.toString()));
-    return tx.hash;
-  }
-
-  requestTokens(body: any) {
-    return { result: true };
-  }
-
-  async mint(body: Mint): Promise<string> {
-    const signedContract = this.contract.connect(this.wallet);
-    const mint = await signedContract.mint(body.address, ethers.utils.parseEther(body.amount));
-    const tx = await mint.wait(1);
-    return tx;
-  }
-
-  async delegate(body: VotePower): Promise<string> {
-    const signedContract = this.contract.connect(this.wallet);
-    const delegate = await signedContract.delegate(body.address);
-    const tx = await delegate.wait(1);
-    return tx;
-  }
-
-  async getVote(address: string) {
-    const signedContract = this.contract.connect(this.wallet);
-    const voteNumber = await signedContract.getVotes(address);
-    return voteNumber.toString();
-  }
-
-  async votePowerSpent(address: string) {
-    const signedContract = this.ballContract.connect(this.wallet);
-    const votePowerSpent = await signedContract.votePowerSpent(address);
-    return votePowerSpent.toString();
-  }
-
-  async getVotePower(address: string) {
-    const signedContract = this.ballContract.connect(this.wallet);
-    const votePower = await signedContract.votePower(address);
-    return votePower.toString();
-  }
-
-  async postVote(body: CastVote): Promise<string> {
-    const signedContract = this.ballContract.connect(this.wallet);
-    const voting = await signedContract.vote(
-      body.proposalIndex,
-      ethers.utils.parseEther(body.amount)
-    );
-    const tx = await voting.wait(1);
-    return tx;
-  }
-
-  async getProposal(): Promise<any> {
-    const signedContract = this.ballContract.connect(this.wallet);
-    for(let index = 0; index < 3; index++) {
-      const proposal = await signedContract.proposals(index);
-      const proposalObj: Proposal = {
-        name: ethers.utils.parseBytes32String(proposal.name),
-        voteCount: ethers.utils.formatEther(proposal.voteCount)
-      };
-      this.proposal.push(proposalObj);
-    }
-    return this.proposal;
-  }
-
-  getBallotAddress() {
-    return {result: CONTRACT_BALLOT_ADDRESS};
-  }
+  
 }
-
 
